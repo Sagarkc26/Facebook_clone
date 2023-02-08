ca74266d7699fbc7c83ea74193087bcdf0c427b0
Sagar k.c <sagarkc45172@gmail.com>
2023-02-08 22:38:05 +0545

    update

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,34 +6,68 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Menu",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Menu",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.settings),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(Icons.search),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: const [
+                  CircleAvatar(),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Name",
+                    style: TextStyle(fontSize: 18),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "All shortcuts",
+                style: TextStyle(fontSize: 20),
+              ),
+              GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemCount: 16,
+                itemBuilder: (context, index) => const Card(
+                  color: Colors.red,
                 ),
-                Row(
-                  children: const [
-                    Icon(Icons.settings),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Icon(Icons.search),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text("h")
-          ]),
+              )
+            ]),
+          ),
         ),
       ),
     );
